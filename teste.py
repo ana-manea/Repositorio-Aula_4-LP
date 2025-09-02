5cf2bf275517c35b7a428942d4961ca4a17c56af
ana-manea <ana_manea@hotmail.com>
2025-09-02 10:33:29 -0300

    Atividade 1

---
+++ teste.py
@@ -0,0 +1,16 @@
+soma = 0
+subt = 0
+
+n1 = int(input("número 1: "))
+n2 = int(input("número 2: "))
+
+soma = n1 + n2
+
+print(f"\nSoma dos números {n1} + {n2} = {soma}")
+
+subt = n1 - n2
+
+print(f"\nSubtração dos números {n1} - {n2} = {subt}")
+
+print(f"\nA raíz quadrada de {n1} é igual a {n1**1/2}")
+print(f"A raíz quadrada de {n2} é igual a {n2**1/2}")
