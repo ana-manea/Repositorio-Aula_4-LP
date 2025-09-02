cd3be9f657f981ad95cda87c749d273585e93343
ana-manea <ana_manea@hotmail.com>
2025-09-02 11:57:00 -0300

    Atualização calculadora

--- teste.py
+++ teste.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template, request
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('calculator.html')
+    return render_template('index.html')
 
 @app.route('/calcular', methods=['POST'])
 def calcular():
-  num1 = float(request.form['num1'])
-  num2 = float(request.form['num2'])
-  operacao = request.form['operacao']
+    try:
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operacao = request.form['operacao']
 
-  resultado = None
+        resultado = None
+        erro = None
 
-  if operacao == 'soma':
-      resultado = num1 + num2
-  elif operacao == 'subtracao':
-      resultado = num1 - num2
-  elif operacao == 'multiplicacao':
-      resultado = num1 * num2
-  elif operacao == 'divisao':
-      if num2 != 0:
-          resultado = num1 / num2
+        if operacao == 'soma':
+            resultado = num1 + num2
+        elif operacao == 'subtracao':
+            resultado = num1 - num2
+        elif operacao == 'multiplicacao':
+            resultado = num1 * num2
+        elif operacao == 'divisao':
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                erro = "Divisão por zero não é permitida!"
 
-  return render_template('calculator.html', resultado=resultado, num1=num1, num2=num2, operacao=operacao)
+        return render_template('index.html', resultado=resultado, num1=num1, num2=num2, operacao=operacao, erro=erro)
+    
+    except ValueError:
+        erro = "Por favor, digite números válidos!"
+        return render_template('index.html', erro=erro)
 
-if _name_ == '_main_':
-  app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
